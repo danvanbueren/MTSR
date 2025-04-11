87ac81fb1dd86c7d0497eba0c9e32beaa14d51ed
Daniel Van Bueren <danvb10@gmail.com>
2025-04-11 18:31:06 +0200

    YACL Config boilerplate

--- app/src/main/java/me/danvb10/mtsr/config/ConfigManager.java
+++ app/src/main/java/me/danvb10/mtsr/config/ConfigManager.java
@@ -1,23 +1,6 @@
 package me.danvb10.mtsr.config;
 
-import me.shedaniel.autoconfig.ConfigData;
-import me.shedaniel.autoconfig.annotation.Config;
-import me.shedaniel.autoconfig.annotation.ConfigEntry;
+public class ConfigManager {
 
-
-@SuppressWarnings({"FieldMayBeFinal", "CanBeFinal", "FieldCanBeLocal", "MismatchedQueryAndUpdateOfCollection"})
-@Config(name = "mtsr")
-public class ConfigManager implements ConfigData {
-
-    @ConfigEntry.Gui.Excluded
-    @ConfigEntry.Category("mtsr.general")
-    public int version = 0;
-
-    @ConfigEntry.Gui.Excluded
-    @ConfigEntry.Category("mtsr.general")
-    public boolean displayDebugData = false;
-
-    @ConfigEntry.Category("mtsr.general")
-    public boolean enabled = true;
 
 }
